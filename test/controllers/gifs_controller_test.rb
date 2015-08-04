7d0ec5af23eeb95de9d7138b2dc25d17e6cce217
Jake Cataford <jakecataford@gmail.com>
2015-08-04 21:12:15 +0000

    Add tests

--- test/controllers/gifs_controller_test.rb
+++ test/controllers/gifs_controller_test.rb
@@ -4,8 +4,13 @@ class GifsControllerTest < ActionController::TestCase
   fixtures :gifs
 
   def setup
+    VCR.insert_cassette("gif_requests", :record => :new_episodes)
     @gif = gifs(:gif_with_valid_source_url)
     session[:current_session] = sessions(:one).id
+  end
+
+  def teardown
+    VCR.eject_cassette
   end
 
   test "#index" do
@@ -38,10 +43,76 @@ class GifsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "redirect to composing works" do
+  test "#compose" do
+    gif = gifs(:composing)
+    get :compose, id: gif
+    assert_response :success
+  end
+
+  test "#compose returns json" do
+    gif = gifs(:composing)
+    get :compose, id: gif, format: :json
+    assert_response :success
+  end
+
+  test "queued gifs show progress view" do
+    gif = gifs(:queued)
+    get :progress, id: gif
+    assert_response :success
+  end
+
+  test "#queued gifs show progress view" do
+    gif = gifs(:queued)
+    get :validate, id: gif, format: :json
+    assert_response :success
+  end
+
+  test "#progress" do
+    gif = gifs(:processing)
+    get :progress, id: gif
+    assert_response :success
+  end
+
+  test "#progress returns json" do
+    gif = gifs(:processing)
+    get :progress, id: gif, format: :json
+    assert_response :success
+  end
+
+
+  test "redirect for composing" do
     gif = gifs(:composing)
     get :validate, id: gif
     assert_response :redirect
-    assert_redirected_to controller: :composing
+    assert_redirected_to compose_gif_url(gif)
+  end
+
+  test "redirect for queued" do
+    gif = gifs(:queued)
+    get :validate, id: gif
+    assert_response :redirect
+    assert_redirected_to progress_gif_url(gif)
+  end
+
+  test "redirect for processing" do
+    gif = gifs(:processing)
+    get :validate, id: gif
+    assert_response :redirect
+    assert_redirected_to progress_gif_url(gif)
+  end
+
+  test "redirect for ready" do
+    gif = gifs(:ready)
+    get :validate, id: gif
+    assert_response :redirect
+    assert_redirected_to gif_url(gif)
+  end
+
+  test "json force_refresh on redirect" do
+    gif = gifs(:ready)
+    get :validate, id: gif, format: :json
+    assert_response :success
+    response_json = JSON.parse(@response.body)
+    assert response_json["force_refresh"]
   end
 end
